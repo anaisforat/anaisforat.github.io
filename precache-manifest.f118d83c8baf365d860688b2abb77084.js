676ca029d8f8da753c755698736e0e093d8901d6
aforat <anais.forat@sword-group.com>
2019-01-16 12:43:58 +0100

    vv

--- precache-manifest.f118d83c8baf365d860688b2abb77084.js
+++ precache-manifest.f118d83c8baf365d860688b2abb77084.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
-    "url": "/fonts/Roboto-Medium.4d9f3f9e.eot"
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/fonts/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "b3ed6554c6a8efc567f7",
+    "url": "/js/about.1b2e55ae.js"
+  },
+  {
+    "revision": "9d2a55285644ea1e67a9",
+    "url": "/js/app.d779998f.js"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
     "url": "/robots.txt"
-  },
-  {
-    "revision": "a990f611f2305dc12965f186c2ef2690",
-    "url": "/fonts/Roboto-Light.a990f611.eot"
-  },
-  {
-    "revision": "76b2a754d788188811d1",
-    "url": "/js/app.8f18aedc.js"
   },
   {
     "revision": "3e8b310c10974b90753d",
@@ -24,36 +24,16 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-Regular.ba3dcd89.woff"
   },
   {
-    "revision": "b3ed6554c6a8efc567f7",
-    "url": "/js/about.1b2e55ae.js"
-  },
-  {
-    "revision": "2751ee43015f9884c3642f103b7f70c9",
-    "url": "/fonts/Roboto-Regular.2751ee43.woff2"
-  },
-  {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
-    "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/img/fontawesome-webfont.912ec66d.svg"
-  },
-  {
-    "revision": "c314e0751ef39b83cb2c9d97ed886b52",
-    "url": "/img/Alex.c314e075.jpg"
-  },
-  {
-    "revision": "76992cbb924f5e089e767031562e8cf9",
-    "url": "/img/logo-cait-horizontal.76992cbb.png"
-  },
-  {
     "revision": "8f5415e6d8daa791345ff330d3e0032e",
     "url": "/img/Mathieu.jpg"
   },
   {
-    "revision": "ffb313ddcf3bd5832a2479a4dc5db4fd",
+    "revision": "a58d99f9eb2bdaca4e8b88330416320c",
     "url": "/index.html"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "fc559777cde068ddbcb58d2ad0a8d12b",
@@ -64,32 +44,48 @@ self.__precacheManifest = [
     "url": "/img/arrow_right.8ef6a08c.svg"
   },
   {
+    "revision": "76992cbb924f5e089e767031562e8cf9",
+    "url": "/img/logo-cait-horizontal.76992cbb.png"
+  },
+  {
     "revision": "11911410dca2de148f30954eb2fd5eab",
     "url": "/img/arrow_left.11911410.svg"
-  },
-  {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
-  },
-  {
-    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
-    "url": "/fonts/Roboto-Bold.39b2c303.woff2"
-  },
-  {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
     "url": "/fonts/Roboto-Bold.ecdd509c.eot"
   },
   {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c314e0751ef39b83cb2c9d97ed886b52",
+    "url": "/img/Alex.c314e075.jpg"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/fonts/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/fonts/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
     "revision": "94998475f6aea65f558494802416c1cf",
     "url": "/fonts/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "dfe56a876d0282555d1e2458e278060f",
@@ -100,8 +96,8 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-Thin.954bbdeb.woff2"
   },
   {
-    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
-    "url": "/fonts/Roboto-Bold.e31fcf18.ttf"
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/fonts/Roboto-Light.46e48ce0.ttf"
   },
   {
     "revision": "fc78759e93a6cac50458610e3d9d63a0",
@@ -110,10 +106,6 @@ self.__precacheManifest = [
   {
     "revision": "df7b648ce5356ea1ebce435b3459fd60",
     "url": "/fonts/Roboto-Regular.df7b648c.ttf"
-  },
-  {
-    "revision": "7500519de3d82e33d1587f8042e2afcb",
-    "url": "/fonts/Roboto-Thin.7500519d.woff"
   },
   {
     "revision": "30799efa5bf74129468ad4e257551dc3",
@@ -128,16 +120,24 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-Medium.574fd0b5.woff2"
   },
   {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/fonts/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/fonts/Roboto-Light.a990f611.eot"
+  },
+  {
     "revision": "69f8a0617ac472f78e45841323a3df9e",
     "url": "/fonts/Roboto-Light.69f8a061.woff2"
   },
   {
-    "revision": "46e48ce0628835f68a7369d0254e4283",
-    "url": "/fonts/Roboto-Light.46e48ce0.ttf"
-  },
-  {
     "revision": "3b813c2ae0d04909a33a18d792912ee7",
     "url": "/fonts/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/fonts/Roboto-Bold.e31fcf18.ttf"
   },
   {
     "revision": "dc81817def276b4f21395f7ea5e88dcd",
@@ -148,7 +148,7 @@ self.__precacheManifest = [
     "url": "/css/chunk-vendors.5d3bff16.css"
   },
   {
-    "revision": "76b2a754d788188811d1",
-    "url": "/css/app.0be6691b.css"
+    "revision": "9d2a55285644ea1e67a9",
+    "url": "/css/app.3f8b9c0f.css"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21ca39f02082caf90567dd4e76e30922.js"
+  "/precache-manifest.f118d83c8baf365d860688b2abb77084.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "cait-alpha-js"});
